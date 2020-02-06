30bb023e0f1b6bb3ca13061dccec99033300c7fb
Parker <carlsop5@wwu.edu>
2020-02-05 18:34:08 -0800

    added functions for getting neighbors

--- Board.java
+++ Board.java
@@ -12,8 +12,17 @@ public class Board<Room> {
     }
 
     //This function adds an edge, from source to destination
+    //addEdge is what you use to add a new room to the graph,
+    //it utilizes addRoom so that you can simultaneously develop
+    //pathways between new and existing rooms
     public void addEdge(Room source, Room Destination){
 
+    }
+
+    //This functions returns an arraylist consisting of all neighbors to a room
+    public ArrayList<Room> getNeighbors(Room node){
+        ArrayList<Room> neighbors = new ArrayList<Room>();
+        return neighbors;
     }
 
 }
