e0a1a58ce1f56e3611e3cd1d1297e1a23bdef8fe
wardj29 <wardj29@wwu.edu>
2020-02-05 18:16:31 -0800

    adding 3 methods

--- Shop.java
+++ Shop.java
@@ -1,3 +1,16 @@
 public class Shop {
 
+
+    public static void actingSuccess(Player player, string rollType){
+
+    }
+
+    public static void sceneWrapBonus(Room room){
+        
+    }
+
+    public static void gainPracticeChip(Player player){
+
+    }
+
 }
