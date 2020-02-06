e05db926ea453eec651e65f59ff768a3ed1dd251
wardj29 <wardj29@wwu.edu>
2020-02-05 18:31:36 -0800

    added specialRules()

--- controller.java
+++ controller.java
@@ -2,19 +2,23 @@ public class controller{
     String currentPlayer;
     int currentDay;
 
-    public static void attemptToAct(Player player, int budget){
+    public static void attemptToAct(int budget){
             // if(succesful){
             //     bank.payout(player, rolltype)
             // }
         
     }
 
-    public static void movePlayer(Player player, Room room){
+    public static void movePlayer(Room room){
 
     }
 
     public static int rollDie(int dieAmount){
         return -1;
+    }
+
+    public static void specialRules(int playerCount){
+
     }
     
     public static void main(String args[]){
