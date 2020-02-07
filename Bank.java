3914af095d5252a2e74cc519ac33a33709b76c1c
wardj29 <wardj29@wwu.edu>
2020-02-06 17:09:35 -0800

    adding variables for upgrading

--- Bank.java
+++ Bank.java
@@ -1,5 +1,16 @@
 public class Bank{
-    
+
+    //there is probably a better way to do this
+    int rankTwoCostDollars = 4;
+    int rankTwoCostCredits = 5;
+    int rankThreeCostDollars = 10;
+    int rankThreeCostCredits = 10;
+    int rankFourCostDollars = 18;
+    int rankFourCostDollars = 15;
+    int rankFiveCostDollars = 28;
+    int rankFiveCostDollars = 20;
+    int rankSixCostDollars = 40;
+    int rankSixCostDollars = 25;
 
     public static void actingSuccess(Player player, String rollType){
 
@@ -10,6 +21,10 @@ public class Bank{
     }
 
     public static void gainPracticeChip(Player player){
+
+    }
+
+    public static void upgrade(){
 
     }
 
