87978d700c3fc09c737913a7ed4b384393ad6c48
wardj29 <wardj29@wwu.edu>
2020-02-06 16:40:31 -0800

    renamed

---
+++ Bank.java
@@ -0,0 +1,16 @@
+public class Bank{
+    
+
+    public static void actingSuccess(Player player, String rollType){
+
+    }
+
+    public static void sceneWrapBonus(Room room){
+        
+    }
+
+    public static void gainPracticeChip(Player player){
+
+    }
+
+}
---
+++ Controller.java
@@ -0,0 +1,33 @@
+public class Controller{
+    String currentPlayer;
+    int currentDay;
+    int maxDays;
+
+
+    public static void attemptToAct(int budget){
+            // if(succesful){
+            //     bank.payout(player, rolltype)
+            // }
+        
+    }
+
+    public static void movePlayer(Room room){
+
+    }
+
+    public static int rollDie(int dieAmount){
+        return -1;
+    }
+
+    public static void specialRules(int playerCount){
+
+    }
+    
+    public static void main(String args[]){
+        //setup board
+        //play game
+        //end game
+    
+    }
+
+}
