9aa8e1c6db930d86484239509c8c03369a128082
wardj29 <wardj29@wwu.edu>
2020-02-07 17:23:06 -0800

    added getters and setters

--- Player.java
+++ Player.java
@@ -7,5 +7,69 @@ public class Player {
     Room currentRoom;
     int practiceChips;
     String roleType;
+
+
+    public String getName(Player player){
+        return "a";
+        //return player.name;
+    }
+
+    public int getDollars(Player player){
+        return -1;
+        //return player.dollars;
+    }
+
+    public int getCredits(Player player){
+        return -1;
+        //return player.credits;
+    }
+
+    public int getRank(Player player){
+        return -1;
+        //return player.rank;
+    }
+
+    public void getCurrentRole(Player player){
+        //will return type Role
+        //return player.currentRole;
+    }
+
+    public void getCurrentRoom(Player player){
+        //will return type Room
+        //return player.currentRole;
+    }
+
+    public String getRoleType(Player player){
+        return "a";
+        //return player.roleType;
+    }
+
+    public void setName(Player player, String name){
+        //set player.name = name;
+    }
+
+    public void addDollars(Player player, int dollars){
+        //set player.dollars += dollars;
+    }
+
+    public void addCredits(Player player, int credits){
+        //set player.credits += player.credits;
+    }
+
+    public void setRank(Player player, int rank){
+        //set player.rank = rank;
+    }
+
+    public void setCurrentRole(Player player, Role role){
+        //set player.currentRole = role;
+    }
+
+    public void setCurrentRoom(Player player, Room room){
+        //set player.currentRoom = room;
+    }
+
+    public void setRoleType(Player player, String rollType){
+        //set player.roleType = rollType;
+    }
 }
 
