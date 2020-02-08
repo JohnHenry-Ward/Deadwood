f885c3e4b3828b360e27cf8783835068bf86ed04
ParkLC <60720992+ParkLC@users.noreply.github.com>
2020-02-07 17:40:30 -0800

    Update Board.java
    added more skeleton code

--- Board.java
+++ Board.java
@@ -3,20 +3,20 @@ import java.util.*;
 public class Board<Room> {
     Room trailer;
     Room castingOffice;
+    Bank bank;
 
     //The board will be represented by a map
     private Map<Room, List<Room> > map = new HashMap<>();
 
     public void addRoom(Room s){
-
+        //This is what develops the node within the map, a helper function for addPath
     }
 
-    //This function adds an edge, from source to destination
-    //addEdge is what you use to add a new room to the graph,
-    //it utilizes addRoom so that you can simultaneously develop
-    //pathways between new and existing rooms
-    public void addEdge(Room source, Room Destination){
-
+    public void addPath(Room source, Room Destination){
+        //This function adds a path, from source to destination
+        //addPath is what you use to add a new room to the graph,
+        //it utilizes addRoom so that you can simultaneously develop
+        //pathways between new and existing rooms
     }
 
     //This functions returns an arraylist consisting of all neighbors to a room
@@ -25,5 +25,23 @@ public class Board<Room> {
         return neighbors;
     }
 
+    public Room getTrailer(){
+        //Returns the Room object for the trailer
+        return trailer;
+    }
+
+    public Room getCastingOffice(){
+        //Returns the Room object for the casting office
+        return castingOffice;
+    }
+
+    public Bank getBank(){
+        //returns the currently used bank object
+        return bank;
+    }
+
+    public void setBank(Bank b){
+        //When game is initialized, this will set which bank is used depending on number of players
+    }
 }
 
