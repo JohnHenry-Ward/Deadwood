51e977bbb95d02f16c3eca68d0a1b303e166a2b7
ParkLC <60720992+ParkLC@users.noreply.github.com>
2020-02-07 17:42:12 -0800

    Update Role.java
    added getters and setters

--- Role.java
+++ Role.java
@@ -2,4 +2,31 @@ public class Role {
     String name;
     int rank;
     Player currentPlayer;
+
+    public String getName(){
+        //return name of role
+        return name;
+    }
+
+    public void setName(String s){
+        //Sets the name(String s) for this current role, used in initialization
+    }
+
+    public int getRank(){
+        //Returns the rank required to acquire this role
+        return rank;
+    }
+
+    public void setRank(int n){
+        //Sets the rank(int n) for the role, used in initialization
+    }
+
+    public Player getPlayer(){
+        //Returns the current player that has this role acquired
+        return currentPlayer;
+    }
+
+    public void setPlayer(Player p){
+        //Links a player(Player p) to this current role
+    }
 }
