b6dec963021c61b62d150af2a71ce17bb072d944
carlsop5 <carlsop5@wwu.edu>
2020-02-12 02:11:10 -0800

    Made the code for the graph containing the board, and populated some minor methods in Card.java

--- Board.java
+++ Board.java
@@ -10,18 +10,33 @@ public class Board<Room> {
 
     public void addRoom(Room s){
         //This is what develops the node within the map, a helper function for addPath
+        map.put(s, new LinkedList<Room>());
     }
 
-    public void addPath(Room source, Room Destination){
+    public void addPath(Room source, Room destination){
         //This function adds a path, from source to destination
         //addPath is what you use to add a new room to the graph,
         //it utilizes addRoom so that you can simultaneously develop
         //pathways between new and existing rooms
+        if(!map.containsKey(source))
+            addRoom(source);
+
+        if(!map.containsKey(destination))
+            addRoom(destination);
+
+        map.get(source).add(destination);
+        map.get(destination).add(source);
     }
 
     //This functions returns an arraylist consisting of all neighbors to a room
     public ArrayList<Room> getNeighbors(Room node){
         ArrayList<Room> neighbors = new ArrayList<Room>();
+
+        for(Room v : map.keySet()){
+            if(map.get(v).contains(node))
+                neighbors.add(v);
+        }
+
         return neighbors;
     }
 
--- Card.java
+++ Card.java
@@ -16,6 +16,7 @@ public class Card {
 
     public void setName(String s){
         //Sets name of card, used in initialization
+        name = s;
     }
 
     public int getBudget(){
@@ -25,23 +26,30 @@ public class Card {
 
     public void setBudget(int n){
         //Sets the budget of card, used in initialization
+        budget = n;
     }
 
     public Role[] getRoles(){
         //Returns an array, containing the roles on the card
+        if(roles.length <= 0)
+        return null;
+        else
         return roles;
     }
 
     public void setRoles(Role a, Role b, Role c){
         //This method is used for setting up 3 roles on a card, all iterations used in initialization
+        roles = new Role[] {a, b, c};
     }
 
     public void setRoles(Role a, Role b){
         //Overloaded method of upper one, lets you set just 2 roles, used in initialization
+        roles = new Role[] {a, b};
     }
 
     public void setRoles(Role a){
         //Overloaded method of upper ones, lets you set just 1 role, used in initialization
+        roles = new Role[] {a};
     }
     
 }
