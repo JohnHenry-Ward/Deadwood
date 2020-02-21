eae787002cd22d61fed7f4b8054421c93bbdd50b
wardj29 <wardj29@wwu.edu>
2020-02-20 16:31:15 -0800

    cleaned up/commented

--- Role.java
+++ Role.java
@@ -4,36 +4,34 @@ public class Role {
     private Player currentPlayer = null;
     private boolean roleAvailable = true;
 
-    public String getName(){
-        return name;
-    }
-
     public Role(String n, int r){
         name = n;
         rank = r;
         currentPlayer = null;
         roleAvailable = true;
     }
+    
+    public String getName(){
+        return name;
+    }
 
     public int getRank(){
-        //Returns the rank required to acquire this role
         return rank;
     }
 
-
     public Player getPlayer(){
         return currentPlayer;
-    }
-
-    public void setRank(Role role, int rank){
-        //Sets the rank(int rank) for the role, used in initialization
-        role.rank = rank;
     }
 
     public void setPlayer(Player player){
         currentPlayer = player;
     }
 
+    /* role only available if
+     * room is unwrapped
+     * player not already on role
+     * role rank is less than or equal to player rank
+     */
     public void setRoleAvailable(boolean avail){
         roleAvailable = avail;
     }
